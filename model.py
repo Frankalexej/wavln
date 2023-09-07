22c5149ebbc5b8c1ca91ad36520e45bed0cefec9
ldlmdl <ldlmdl@hku.hk>
2023-09-07 20:23:14 +0800

    20230907 simple-1

--- model.py
+++ model.py
@@ -206,12 +206,12 @@ class PhxLearner(Module):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-    def forward(self, inputs, in_mask):
+    def forward(self, inputs, input_lens, in_mask):
         # inputs : batch_size * time_steps * in_size
         batch_size = inputs.size(0)
         dec_hid, init_in = self.decoder.inits(batch_size=batch_size, device=self.device)
 
-        enc_out = self.encoder(inputs, in_mask)
+        enc_out = self.encoder(inputs, input_lens, in_mask)
         dec_out, attn_w = self.decoder(enc_out, in_mask, init_in, dec_hid)
         
         return dec_out, attn_w
@@ -223,6 +223,130 @@ class PhxLearner(Module):
 
 
 
+
+
+
+class OnlyRNNEncoder(Module): 
+    def __init__(self, size_list, num_layers=1):
+        # size_list = [39, 64, 16, 3]
+        super(OnlyRNNEncoder, self).__init__()
+        self.rnn = nn.LSTM(input_size=size_list[0], hidden_size=size_list[3], num_layers=num_layers, batch_first=True)
+        self.act = nn.Tanh()
+
+    def forward(self, inputs, inputs_lens, in_mask=None, hidden=None):
+        """
+        Args:
+            inputs: input data (B, L, I)
+            inputs_lens: input lengths
+            in_mask: masking (B, L), abolished, since now we have packing and padding
+            hidden: HM_LSTM, abolished
+        """
+        # enc_x = self.lin_1(inputs) # (B, L, I0) -> (B, L, I1)
+        enc_x = inputs
+
+        enc_x = pack_padded_sequence(enc_x, inputs_lens, batch_first=True, enforce_sorted=False)
+
+        enc_x, (hn, cn) = self.rnn(enc_x)  # (B, L, I1) -> (B, L, I2)
+
+        enc_x, _ = pad_packed_sequence(enc_x, batch_first=True)
+
+        # enc_x = self.lin_2(enc_x) # (B, L, I2) -> (B, L, I3)
+        enc_x = self.act(enc_x)
+
+        return enc_x
+    
+    def encode(self, inputs, inputs_lens, in_mask=None, hidden=None): 
+        # enc_x = self.lin_1(inputs) # (B, L, I0) -> (B, L, I1)
+        enc_x = inputs
+
+        enc_x = pack_padded_sequence(enc_x, inputs_lens, batch_first=True, enforce_sorted=False)
+
+        enc_x, (hn, cn) = self.rnn(enc_x)  # (B, L, I1) -> (B, L, I2)
+
+        enc_x, _ = pad_packed_sequence(enc_x, batch_first=True)
+
+        # enc_x = self.lin_2(enc_x) # (B, L, I2) -> (B, L, I3)
+        enc_x = self.act(enc_x)
+
+        return enc_x
+
+class SimperRNNDecoder(Module): 
+    def __init__(self, size_list, num_layers=1):
+        # size_list = [13, 64, 16, 3]: similar to encoder, just layer 0 different
+        super(SimperRNNDecoder, self).__init__()
+        self.lin_1 = LinearPack(in_dim=size_list[0], out_dim=size_list[1])  # NOTE: we use out size (last in size_list) as input size to lin, because we will take the direct output from last layer in dec. 
+        self.rnn = nn.LSTM(size_list[1], size_list[3], num_layers=num_layers, batch_first=True)
+        # self.lin_2 = LinearPack(in_dim=size_list[2], out_dim=size_list[3])
+        self.attention = ScaledDotProductAttention(q_in=size_list[3], kv_in=size_list[3], qk_out=size_list[3], v_out=size_list[3])
+        self.lin_3 = LinearPack(in_dim=size_list[3], out_dim=size_list[0])
+        self.act = nn.Tanh()
+
+        # vars
+        self.num_layers = num_layers
+        self.size_list = size_list
+
+    def inits(self, batch_size, device): 
+        h0 = torch.zeros((self.num_layers, batch_size, self.size_list[3]), dtype=torch.float, device=device, requires_grad=False)
+        c0 = torch.zeros((self.num_layers, batch_size, self.size_list[3]), dtype=torch.float, device=device, requires_grad=False)
+        hidden = (h0, c0)
+        dec_in_token = torch.zeros((batch_size, 1, self.size_list[0]), dtype=torch.float, device=device, requires_grad=False)
+        return hidden, dec_in_token
+
+    def forward(self, hid_r, in_mask, init_in, hidden):
+        # Attention decoder
+        length = hid_r.size(1) # get length
+
+        dec_in_token = init_in
+
+        outputs = []
+        attention_weights = []
+        for t in range(length):
+            dec_x = self.lin_1(dec_in_token)
+            dec_x, hidden = self.rnn(dec_x, hidden)
+            # dec_x = self.lin_2(dec_x)
+            dec_x, attention_weight = self.attention(dec_x, hid_r, hid_r, in_mask.unsqueeze(1))    # unsqueeze mask here for broadcast
+            dec_x = self.lin_3(dec_x)
+            dec_x = self.act(dec_x)
+            outputs.append(dec_x)
+            attention_weights.append(attention_weight)
+
+            # Use the current output as the next input token
+            dec_in_token = dec_x
+
+        outputs = torch.stack(outputs, dim=1)   # stack along length dim
+        attention_weights = torch.stack(attention_weights, dim=1)
+        outputs = outputs.squeeze(2)
+        attention_weights = attention_weights.squeeze(2)
+        outputs = mask_it(outputs, in_mask)     # test! I think it should be correct. 
+        
+        return outputs, attention_weights
+
+
+
+class SimplerPhxLearner(Module):
+    def __init__(self, enc_size_list, dec_size_list, num_layers=1):
+        # input = (batch_size, time_steps, in_size); 
+        super(SimplerPhxLearner, self).__init__()
+
+        self.encoder = OnlyRNNEncoder(size_list=enc_size_list, num_layers=num_layers)
+        self.decoder = SimperRNNDecoder(size_list=dec_size_list, num_layers=num_layers)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    def forward(self, inputs, input_lens, in_mask):
+        # inputs : batch_size * time_steps * in_size
+        batch_size = inputs.size(0)
+        dec_hid, init_in = self.decoder.inits(batch_size=batch_size, device=self.device)
+
+        enc_out = self.encoder(inputs, input_lens, in_mask)
+        dec_out, attn_w = self.decoder(enc_out, in_mask, init_in, dec_hid)
+        
+        return dec_out, attn_w
+    
+    def encode(self, inputs, in_mask): 
+        batch_size = inputs.size(0)
+        hidden = self.encoder.inits(batch_size=batch_size, device=self.device)
+        return self.encoder.encode(inputs, in_mask, hidden)
 
 
 
