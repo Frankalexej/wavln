4519dce219a49db113f694366262f658168a0ecf
ldlmdl <ldlmdl@hku.hk>
2024-01-23 11:29:30 +0800

    20240123

--- layers.py
+++ layers.py
@@ -153,9 +153,9 @@ class LinearPack(nn.Module):
         super(LinearPack, self).__init__()
         self.linear = nn.Linear(in_dim, out_dim)
         # self.layernorm = nn.LayerNorm(out_dim)  # Layer normalization
-        self.relu = nn.ReLU()
+        # self.relu = nn.ReLU()
         # self.relu = nn.LeakyReLU()
-        # self.relu = nn.Tanh()
+        self.relu = nn.Tanh()
         self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x):
