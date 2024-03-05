8a578ec565f251f8d57c0b6ee7674778613d4125
FrankAlexej <tt115889@connect.hku.hk>
2024-03-05 10:57:48 +0800

    20240305 + n3

--- C_0A_integrate_eval_runner.sh
+++ C_0A_integrate_eval_runner.sh
@@ -20,13 +20,11 @@ echo "Timestamp: $ts"
 
 # Loop from 1 to 10, incrementing by 1
 # Loop over each combination of arguments
-for m in "${ms[@]}"; do
-    for c in "${cs[@]}"; do
-        # Randomly select a GPU between 0 and 8
-        gpu=$((RANDOM % 9))
-        # Run the Python script with the current combination of arguments in the background
-        python C_0A_integrate_eval.py -ts "$ts" -m "$m" -cd "$c" -gpu "$gpu" &
-    done
+for c in "${cs[@]}"; do
+    # Randomly select a GPU between 0 and 8
+    gpu=$((RANDOM % 9))
+    # Run the Python script with the current combination of arguments in the background
+    python C_0A_integrate_eval.py -ts "$ts" -m "$m" -cd "$c" -gpu "$gpu" &
 done
 
 # Wait for all background processes to finish
