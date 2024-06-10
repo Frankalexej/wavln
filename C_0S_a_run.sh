9df60de28542aa52fccb435fb40eaaaf981200aa
elftaomi <460214560@qq.com>
2024-06-10 18:37:01 +0800

    20240610 + 15

--- C_0S_a_run.sh
+++ C_0S_a_run.sh
@@ -22,13 +22,13 @@ echo "Timestamp: $ts"
 # Loop from 1 to 10, incrementing by 1
 for (( i=1; i<=5; i++ )); do
     # Loop over each combination of arguments
-    python C_0S_run.py -ts "$ts-$i" -dp
+    python C_0S_a_run.py -ts "$ts-$i" -dp
     for m in "${ms[@]}"; do
         for c in "${cs[@]}"; do
             # Randomly select a GPU between 0 and 8
             gpu=$((RANDOM % 9))
             # Run the Python script with the current combination of arguments in the background
-            python C_0S_run.py -ts "$ts-$i" -m "$m" -cd "$c" -gpu "$gpu" &
+            python C_0S_a_run.py -ts "$ts-$i" -m "$m" -cd "$c" -gpu "$gpu" &
         done
     done
 done
