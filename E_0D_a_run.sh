91d7258bdeabdfbf07f9050f3bab8e7c8137ab41
Frankalexej <tt115889@connect.hku.hk>
2024-12-10 01:43:03 +0800

    20241210 + 9

--- E_0D_a_run.sh
+++ E_0D_a_run.sh
@@ -15,7 +15,8 @@ ms=('recon4-phi' 'recon8-phi' 'recon16-phi' 'recon32-phi' 'recon48-phi' 'recon64
 cs=('b')
 
 # Generate a 10-digit random number
-ts=$(date +"%m%d%H%M%S")
+# ts=$(date +"%m%d%H%M%S")
+ts='1210012614'
 # ts='0611193546'
 # ts='0902011400'
 echo "Timestamp: $ts"
