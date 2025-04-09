df288c52d8b7b2c2af3072e10fbda65a2c813bc1
Frankalexej <tt115889@connect.hku.hk>
2025-04-09 17:03:09 +0800

    20250409 + 1

--- slurmjob.sh
+++ slurmjob.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=sandbox_test
-#SBATCH --output=/nfs/franklhtan/logs/sandbox_test.out
-#SBATCH --error=/nfs/franklhtan/logs/sandbox_test.err
+#SBATCH --output=/mnt/storage/franklhtan/wavln/test_job.out
+#SBATCH --error=/mnt/storage/franklhtan/wavln/test_job.err
 #SBATCH --time=00:10:00
 #SBATCH --partition=compute
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --chdir="/nfs/franklhtan/projects/wavln/scripts/"
+#SBATCH --chdir="mnt/franklhtan/projects/wavln/scripts/"
 
 singularity exec --nv --fakeroot --writable home/franklhtan/ubuntu \
 bash -c "source /opt/anaconda3/etc/profile.d/conda.sh && conda activate wavln && python test.py"
